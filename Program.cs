c924ce03ef124199a0348be48bf486128d383356
Victor <victor.melocariati@gmail.com>
2020-11-12 15:09:15 -0300

    Finalizado igual o do professor

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Logica_de_programação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUm;
+            int valorDois:
+
+            //aqui vai o codigo
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("----------------------");
+            Console.Write("Dígite o primeiro valor: ")
+            int valorUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ")
+            int valorDois = int.Parse(Console.ReadLine());
+
+            //Processamento
+            int soma = valorUm + valorDois;
+
+            //Exibir
+            Console.WriteLine("A Soma de "+valorUm+" + "+valorDois+" = "+soma);
+            
+
+        }
+    }
+}
